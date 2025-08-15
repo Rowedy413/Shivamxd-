7e55bb77cff02008908b772cfa2e63bd12d5ca48
Rowedy413 <rinkudadar1234@gmail.com>
2025-08-15 11:23:02 +0530

    Update app.py

--- app.py
+++ app.py
@@ -336,3 +336,6 @@ HTML = '''
           .catch(e=>alert('Error downloading file'));
       }
       </
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
